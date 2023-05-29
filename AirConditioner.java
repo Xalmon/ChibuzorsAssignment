059fd177221fec946c40500dce26992cace32dc8
Xalmon <xalmonoyefule@gmail.com>
2023-05-29 06:44:34 +0100

    MrChibuzorsAssignment

---
+++ AirConditioner.java
@@ -0,0 +1,43 @@
+	public class AirConditioner{
+	
+		private boolean isOn;
+		private String productName;
+		private int temperature;
+		
+		public AirConditioner(){
+			String name = productName;
+			}
+		public void decreaseTemperature(){
+			temperature = temperature;
+			}
+		public String getProductName(){
+			String getProductName = productName;		
+			return productName;
+			}		
+		public int getTemperature(){
+			int getTemperature = temperature;
+			return temperature;
+			}
+		public void increaseTemperature(){
+			temperature = temperature;
+			}
+		public boolean isOn(){
+			isOn = true;
+			isOn = false;
+			return isOn;
+			}
+		public void setOn(){
+			
+			}
+		public String setProductName(){
+			String setProductName = productName;
+			return productName;
+			}
+		public int setTemperature(){
+			int setTemperature = temperature;
+			return temperature;
+			}
+			
+			
+		}
+
